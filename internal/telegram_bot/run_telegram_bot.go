38fd5ef215f4b9090cb745116fcd9e3219226087
angi <angela.scherer@aiza.ch>
2024-05-29 20:01:15 +0200

    optimized code

--- internal/telegram_bot/run_telegram_bot.go
+++ internal/telegram_bot/run_telegram_bot.go
@@ -70,7 +70,7 @@ func RunTelegramBot() {
 		}
 
 		if strings.Contains(update.Message.Text, "/help") {
-			helpInfo := "Usage:\n- Type 'movies' for the 10 latest horror movies \n- Specify a number for more \n   e.g. '15 movies' \n- Add a date: e.g. 'movies 01.01.94' \n- Specify genres:\n   Sci-Fi, Fantasy, Thriller, Animation or Mystery \n   e.g. 'sci-fi movies' \n- Combine options: \n   e.g. '20 sci-fi thriller movies 10.10.19'"
+			helpInfo := "Usage:\n- Type 'movies' for the 10 latest horror movies \n- Specify a number for more \ne.g. '15 movies' \n- Add a date e.g. 'movies 14.06.19' \n- Specify genres: Sci-Fi, Fantasy, Thriller, Animation or Mystery e.g. 'sci-fi movies' \n- Combine options e.g. '20 sci-fi thriller movies 14.06.19'"
 			msg := tgbotapi.NewMessage(update.Message.Chat.ID, helpInfo)
 			_, err = bot.Send(msg)
 			if err != nil {
